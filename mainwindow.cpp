f772b37ca19ff936a311334f5950ebf9cbaa0e47
scimulate <samjacobs@scimulate.com>
2021-12-20 18:02:27 -0500

    Successful tracking off-window mouse, color @ location. etc.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowFlag(Qt::WindowStaysOnTopHint);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +17,45 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    QString text = "Global Position (x,y) = (";
+    text += QString::number(event->globalX());
+    text += ", ";
+    text += QString::number(event->globalY());
+    text += ")";
+    ui->labelPosition->setText(text);
+    path.append(QList<int>{event->globalX(), event->globalY()});
+
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QPixmap something = screen->grabWindow(0,  event->globalX(), event->globalY(), 1, 1);
+
+    QColor pixelValue = something.toImage().pixel(0,0);
+
+    text.clear();
+    text = "Pixel Color (r, g, b) = (";
+    text += QString::number(pixelValue.red());
+    text += ", ";
+    text += QString::number(pixelValue.green());
+    text += ", ";
+    text += QString::number(pixelValue.blue());
+    text += ")";
+    ui->labelRGB->setText(text);
+    ui->labelPixel->setPixmap(something);
+    //std::cout << pixelValue.red() << '\t' << pixelValue.green() << '\t' << pixelValue.blue() << std::endl;;
+
+    //QColor color = something.toImage().color();
+    //std::cout << something.toImage().pixel();
+    //QPixmap something = QScreen::grabWindow(this->winId(), event->globalX(), event->globalY(), 1, 1);
+}
+
+void MainWindow::on_pushButton_pressed()
+{
+    path.clear();
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    std::cout << path.length() << std::endl;
+    ui->labelPixel->setText(QString::number(path.length()));
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QApplication>
+#include <QList>
 #include <QMainWindow>
+#include <QMouseEvent>
+#include <QPoint>
+#include <QPixmap>
+#include <QRgb>
+#include <QScreen>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +22,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+private slots:
+    //void on_pushButton_pressed();
+    //void on_pushButton_released();
+    //void on_pushButton_clicked();
+
+    void on_pushButton_pressed();
+
 private:
     Ui::MainWindow *ui;
+    QList<QList<int>> path;
 };
 #endif // MAINWINDOW_H
