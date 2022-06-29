97b8c7760cbc970cc7a41df1f0887d80dc676208
scimulate <samjacobs@scimulate.com>
2022-06-28 23:00:48 -0400

    Updated About page. Preparing for source code distribution.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,9 +25,6 @@ MainWindow::MainWindow(QWidget *parent)
 
     QAction *aboutQtAct = helpMenu->addAction(tr("About &Qt.."), qApp, &QApplication::aboutQt);
     aboutQtAct->setStatusTip(tr("PlotVISOR was developed using Qt technologies..."));
-
-    QMessageBox *msgbox;
-    msgbox->about(this, tr("PlotVISOR 2022.0 (Evaluation Version)"), tr("Scimulate LLC has released PlotVISOR as freeware. If you find this software useful, please consider purchasing on our <a href=\"https://www.scimulate.com/products/p/plotvisor\">website</a> to remove this message."));
 }
 
 MainWindow::~MainWindow()
@@ -165,7 +162,6 @@ void MainWindow::About()
                        tr("<b>PlotVISOR</b> is a visual assistant for contour plots.<br><br>"
                           "By following the instructions (1) to (4) in the program window, users can map the legend of any contour plot and use it to identify values otherwise difficult to interpret.<br><br>"
                           "If \"???\" is displayed instead of a valid result, disable lighting enhancements (within your host software) and/or remap the calibration curve. PlotVISOR has been configured to accommodate mild lighting variation from rendering, but this may affect results by ±1 legend increment.<br><br>"
-                          "Visit Scimulate LLC on our <a href=\"https://www.scimulate.com\">website</a> and follow us on <a href=\"https://www.linkedin.com/company/scimulate-llc\">LinkedIn</a>! Please contact us to ask questions, report bugs, request features, or provide any other feedback.<br><br>"
-                          "Copyright (c) 2022 Scimulate LLC <br><br>"
-                          "Scimulate, \"Solving the Unsolved\", and the Scimulate logos are trademarks of Scimulate LLC."));
+                          "Please visit our <a href=\"https://www.scimulate.com\">website</a> and follow us on <a href=\"https://www.linkedin.com/company/scimulate-llc\">LinkedIn</a>!<br><br>"
+                          "PlotVISOR is made available to you under the terms of the <a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GPLv3</a>. You are not granted any trademark rights or licenses to the trademarks of Scimulate LLC.<br><br>"));
 }
